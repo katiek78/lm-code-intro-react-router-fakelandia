a413da09ddb28d383525da399877a5bbe42b9ff6
katiek78 <kermodekatie@gmail.com>
2023-03-21 15:12:57 +0000

    feat(App): Add footer

--- client/src/components/MainLayout.tsx
+++ client/src/components/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const MainLayout : React.FC = () => <>
     <Header />
     <main>        
         <Outlet />
     </main>
+    <Footer />
 </>;
 
 export default MainLayout;
