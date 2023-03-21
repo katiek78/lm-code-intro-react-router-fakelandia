69211a9b77b44577bbd91b25ddd7bba22960676d
katiek78 <kermodekatie@gmail.com>
2023-03-21 12:50:09 +0000

    feat(App): Improve styling

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,2 +1,5 @@
-const Home : React.FC = () => <>Home!</>;
+const Home : React.FC = () => <>
+    <p>Welcome to the home of the Justice Department of Fakelandia.</p>
+    <p>Here you can browse a list of recent misdemeanours committed by our citizens, or you can confess to your own misdemeanour.</p>
+</>;
 export default Home;
