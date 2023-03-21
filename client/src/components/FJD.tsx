8e57f279ee69ece223045046fceb3c7797d00793
katiek78 <kermodekatie@gmail.com>
2023-03-21 10:23:44 +0000

    feat: Improve header styling to match UI story

--- client/src/components/FJD.tsx
+++ client/src/components/FJD.tsx
@@ -1,5 +1,5 @@
 const FJD : React.FC = () => <>
-    <div>Fakelandia Justice Department</div>
+    <div className="FJD">Fakelandia Justice Department</div>
 </>;
 
 export default FJD;
--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -2,10 +2,10 @@ import { NavLink } from "react-router-dom";
 
 const Nav : React.FC = () => <>
     <nav>
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/misdemeanours">Misdemeanours</NavLink></li>
-            <li><NavLink to="/confession">Confess To Us</NavLink></li>
+        <ul className='nav--ul'>
+            <li className='nav--li'><NavLink className='nav--a' to="/">Home</NavLink></li>
+            <li className='nav--li'><NavLink className='nav--a' to="/misdemeanours">Misdemeanours</NavLink></li>
+            <li className='nav--li'><NavLink className='nav--a' to="/confession">Confess To Us</NavLink></li>
         </ul>
     </nav>
 </>
