319657179d03ea735b073b180268f8281d58e677
katiek78 <kermodekatie@gmail.com>
2023-03-21 07:56:04 +0000

    test(App): Tidied up test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,13 +1,9 @@
 import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('App renders and user can navigate', async () => {
-   // render(<App />, {wrapper: BrowserRouter})
-    render(<App />)
-    const user = userEvent.setup()
+    render(<App />)    
   
     // default route
     console.log(screen.debug);
