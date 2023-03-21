0ed45432688f33677578a354feb784e992f91f61
katiek78 <kermodekatie@gmail.com>
2023-03-21 15:58:52 +0000

    feat(Nav): Style nav links when active

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,13 +1,21 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, redirect } from "react-router-dom";
 
-const Nav : React.FC = () => <>
-    <nav>
-        <ul className='nav__ul'>
-            <li className='nav__li'><NavLink className='nav__a' to="/">Home</NavLink></li>
-            <li className='nav__li'><NavLink className='nav__a' to="/misdemeanours">Misdemeanours</NavLink></li>
-            <li className='nav__li'><NavLink className='nav__a' to="/confession">Confess To Us</NavLink></li>
-        </ul>
-    </nav>
-</>
+const Nav : React.FC = () => {
+    const activeStyleObject = { color: '#BB00BB' };
+    const nonactiveStyleObject = { color: '#000000' }
+
+    return(<>
+        <nav>
+            <ul className='nav__ul'>
+                <li className='nav__li'><NavLink className='nav__a' to="/" style={({ isActive }) =>
+                isActive ? activeStyleObject : nonactiveStyleObject}>Home</NavLink></li>
+                <li className='nav__li'><NavLink className='nav__a' to="/misdemeanours"  style={({ isActive }) =>
+                isActive ? activeStyleObject : nonactiveStyleObject}>Misdemeanours</NavLink></li>
+                <li className='nav__li'><NavLink className='nav__a' to="/confession"  style={({ isActive }) =>
+                isActive ? activeStyleObject : nonactiveStyleObject}>Confess To Us</NavLink></li>
+            </ul>
+        </nav>
+    </>);
+}
 
 export default Nav;
