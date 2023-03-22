0cad0bbb53ab956481442035a2bbb1912d0a45e1
katiek78 <kermodekatie@gmail.com>
2023-03-22 19:41:08 +0000

    feat(Misdemeanours): Improve grid styling

--- client/src/components/MisdemeanourDisplay.tsx
+++ client/src/components/MisdemeanourDisplay.tsx
@@ -6,17 +6,17 @@ const MisdemeanourDisplay : React.FC = () => {
 
     return(
     <>
-    <p className='misdemeanour__filter'>Filter</p>
-    <div className='misdemeanourContainer'>                      
-            <p className='grid__item heading citizenId__heading'>Citizen ID</p>                  
-            <p className='grid__item heading date__heading'>Date</p>                    
+    <div className="container"><p className='misdemeanour__filter'>Filter</p></div>
+    <div className='container'>                      
+            <p className='grid__line heading citizenId__heading'>Citizen ID</p>                  
+            <p className='grid__line heading date__heading'>Date</p>                    
             
-            <p className='grid__item heading misdemeanour__heading'>Misdemeanour</p>            
+            <p className='grid__line heading misdemeanour__heading'>Misdemeanour</p>            
             <p className='heading punishment__heading'>Punishment idea</p>
             {misdemeanours.map((m) => (<>
-            <p className='grid__item grid__column1'>{m.citizenId}</p>
-            <p className='grid__item grid__column2'>{m.date}</p>
-            <p className='grid__item grid__column3'>{m.misdemeanour} {MISDEMEANOUR_EMOJIS[MISDEMEANOURS.indexOf(m.misdemeanour)]}</p>
+            <p className='grid__line grid__column1'>{m.citizenId}</p>
+            <p className='grid__line grid__column2'>{m.date}</p>
+            <p className='grid__line grid__column3'>{m.misdemeanour} {MISDEMEANOUR_EMOJIS[MISDEMEANOURS.indexOf(m.misdemeanour)]}</p>
             <p></p></>))}    
     </div>
 
