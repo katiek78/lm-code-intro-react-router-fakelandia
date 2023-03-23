d207f749a15249fe42f4006cee037b97ad8be2c7
katiek78 <kermodekatie@gmail.com>
2023-03-23 09:22:39 +0000

    fix(MisdemeanourDisplay): Add unique key to each child in list

--- client/src/components/MisdemeanourDisplay.tsx
+++ client/src/components/MisdemeanourDisplay.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useMisdemeanours } from "./MisdemeanourContext";
 import { MISDEMEANOURS, MISDEMEANOUR_EMOJIS } from "../../types/misdemeanours.types";
 
@@ -8,9 +8,9 @@ const MisdemeanourDisplay : React.FC = () => {
 
     return(
     <>
-    <div className="container"><p className='misdemeanour__filter'><select id='filterMisdemeanourKind' placeholder="Filter" onChange={(e) => setFilterKind(e.target.value)}>
-    <option value="" disabled selected>Filter</option>
-    {MISDEMEANOURS.map((m) => <option value={m}>{m}</option>)}
+    <div className="container"><p className='misdemeanour__filter'><select id='filterMisdemeanourKind' defaultValue="" placeholder="Filter" onChange={(e) => setFilterKind(e.target.value)}>
+    <option value="" disabled>Filter</option>
+    {MISDEMEANOURS.map((m, index) => <option key={'option' + index} value={m}>{m}</option>)}
     <option value="all">** show all **</option>
     </select></p></div>
     <div className='container'>                      
@@ -24,11 +24,12 @@ const MisdemeanourDisplay : React.FC = () => {
                 const selectBox: HTMLSelectElement | null =  document.getElementsByTagName('select')[0];
                 if (!selectBox) return true;
                 return selectBox.value==='all' || selectBox.value === '' || selectBox.value===m.misdemeanour})
-            .map((m, index) => (<>
+            .map((m, index) => (<React.Fragment key={'gridItem' + index}>
             <p className='grid__item grid__line grid__column1'>{m.citizenId}</p>
             <p className='grid__item grid__line grid__column2'>{m.date}</p>
             <p className='grid__item grid__line grid__column3'>{m.misdemeanour} {MISDEMEANOUR_EMOJIS[MISDEMEANOURS.indexOf(m.misdemeanour)]}</p>
-            <p className='grid__item grid__column4'><img src={`https://picsum.photos/150/100?random=${m.citizenId}`} /></p></>))}    
+            <p className='grid__item grid__column4'><img src={`https://picsum.photos/150/100?random=${m.citizenId}`} /></p>
+            </React.Fragment>))}    
     </div>
 
     <div>
