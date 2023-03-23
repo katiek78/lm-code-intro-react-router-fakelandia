88a566deabb1448a94921ac1b97d6c64e96ea66d
katiek78 <kermodekatie@gmail.com>
2023-03-23 17:59:01 +0000

    feat(Confession): Add basic text and subject box to Confession page

--- client/src/components/Confession.test.tsx
+++ client/src/components/Confession.test.tsx
@@ -4,5 +4,5 @@ import Confession from './Confession';
 
 test('renders appropriate text when component is rendered', () => {
     render(<Confession />);    
-    expect(screen.getByText(/Confession time!/i)).toBeInTheDocument();    
+    expect(screen.getByText(/It's very difficult to catch people committing misdemeanours so we appreciate it when citizens confess to us directly./i)).toBeInTheDocument();    
   });
--- client/src/components/Confession.tsx
+++ client/src/components/Confession.tsx
@@ -1,3 +1,16 @@
-const Confession : React.FC = () => <>Confession time!</>;
+const Confession : React.FC = () => <>
+    <div className="confession__container">
+        <p className="confession__intro-text">It's very difficult to catch people committing misdemeanours so we appreciate it when citizens confess to us directly.</p>
+
+        <p className="confession__intro-text">However, if you're just having a hard day and need to vent then you're welcome to contact us here too. Up to you!</p>
+
+
+        <form>
+            <label htmlFor='subjectInput'>Subject</label>
+            <input type="text" id="subjectInput"></input>
+
+        </form>
+    </div>
+</>;
 
 export default Confession;
