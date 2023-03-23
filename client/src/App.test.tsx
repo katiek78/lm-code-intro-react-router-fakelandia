84dcf3489ebf369e97eeb2d50b40fce42b557905
katiek78 <kermodekatie@gmail.com>
2023-03-23 07:17:23 +0000

    test(App): Update app test because of changes in Misdemeanours page and add bad route test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,7 +1,8 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('App renders and user can navigate', async () => {
     render(<App />);
@@ -13,7 +14,7 @@ test('App renders and user can navigate', async () => {
 
     // Misdemeanours route
     await user.click(screen.getByText(/Misdemeanours/i, { selector: 'a'}))
-    expect(screen.getByText(/Misdemeanours!/i)).toBeInTheDocument()
+    await waitFor(() => expect(screen.getByText(/Citizen ID/i)).toBeInTheDocument());
 
     // Confessions route
     await user.click(screen.getByText(/Confess To Us/i, { selector: 'a'}))
@@ -23,5 +24,16 @@ test('App renders and user can navigate', async () => {
     await user.click(screen.getByText(/Home/i, { selector: 'a'}))
     expect(screen.getByText(/Welcome/i)).toBeInTheDocument()
 
-})
+});
+
+test('landing on a bad page', () => {
+    const badRoute = '/some/bad/route'
+      
+    render(
+      <MemoryRouter initialEntries={[badRoute]}>
+        <App />
+      </MemoryRouter>,    
+    )
+    expect(screen.getByText(/Something went wrong.../i)).toBeInTheDocument();
+});
 
