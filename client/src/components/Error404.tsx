9b3e951c39ad3a5f611ed81953281249ef8e421a
katiek78 <kermodekatie@gmail.com>
2023-03-25 14:30:21 +0000

    feat(Error404): Add Fakelandia-relevant text to error message

--- client/src/components/Error404.tsx
+++ client/src/components/Error404.tsx
@@ -1,3 +1,3 @@
-const Error404 : React.FC = () => <>404 - Not found!</>;
+const Error404 : React.FC = () => <><p>404 - Not found! Apologies from the Government of Fakelandia for this inconvenience, which is fortunately not a misdemeanour.</p></>;
 
 export default Error404;
