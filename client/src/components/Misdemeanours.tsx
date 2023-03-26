46a7a65e68908bd16d65798696d2c2e0997c7cb9
katiek78 <kermodekatie@gmail.com>
2023-03-26 08:06:13 +0100

    feat(Misdemeanours): Add Loading message

--- client/src/components/Misdemeanours.tsx
+++ client/src/components/Misdemeanours.tsx
@@ -4,6 +4,6 @@ import MisdemeanourDisplay from "./MisdemeanourDisplay";
 const Misdemeanours: React.FC = () => {
   const {misdemeanours} = useMisdemeanours();
 
-  return <>{misdemeanours.length > 0 && <MisdemeanourDisplay />}</>;
+  return <>{misdemeanours.length > 0 && <MisdemeanourDisplay />}{misdemeanours.length === 0 && <p className="misdemeanour__loading">Loading misdemeanours...</p>}</>;
 };
 export default Misdemeanours;
