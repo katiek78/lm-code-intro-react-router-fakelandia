78fa3a3a7780d5c7ec4adb72f99119b3bd6d4c35
katiek78 <kermodekatie@gmail.com>
2023-03-26 08:02:58 +0100

    refactor: Move MisdemeanourContext consumer

--- client/src/components/MainLayout.tsx
+++ client/src/components/MainLayout.tsx
@@ -1,13 +1,35 @@
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import loadMisdemeanours from "./loadMisdemeanour";
+import { Misdemeanour } from "../types/misdemeanours.types";
+import MisdemeanourContext from "./MisdemeanourContext";
 
-const MainLayout : React.FC = () => <>
-    <Header />
-    <main>        
-        <Outlet />
-    </main>
-    <Footer />
-</>;
+const MainLayout: React.FC = () => {
+  const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
+
+  useEffect(() => {
+    const getMisdemeanours = async () => {
+      const result = await loadMisdemeanours();
+      setMisdemeanours(result.misdemeanours);
+    };
+    getMisdemeanours();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <MisdemeanourContext.Provider
+          value={{ misdemeanours, setMisdemeanours }}
+        >
+          <Outlet />
+        </MisdemeanourContext.Provider>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default MainLayout;
--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -5,25 +5,7 @@ import Misdemeanours from './Misdemeanours';
 import Error404 from './Error404';
 import MainLayout from './MainLayout';
 
-import { useEffect, useState } from 'react';
-import loadMisdemeanours from './loadMisdemeanour';
-import { Misdemeanour } from '../types/misdemeanours.types';
-import MisdemeanourContext from './MisdemeanourContext';
-
-const Router: React.FC = () => {
-    const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
-
-    useEffect(() => {  
-      const getMisdemeanours = async () => {
-          const result = await loadMisdemeanours();
-          setMisdemeanours(result.misdemeanours);                                                   
-      };    
-      getMisdemeanours();    
-    }, []);
-  
-
-   return (
-    <MisdemeanourContext.Provider value={{misdemeanours, setMisdemeanours}}>
+const Router: React.FC = () =>     
         <Routes>
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<Home />}></Route>
@@ -31,9 +13,6 @@ const Router: React.FC = () => {
                 <Route path="/misdemeanours" element={<Misdemeanours />}></Route>
                 <Route path="*" element={<Error404></Error404>} />
             </Route>
-        </Routes>
-    </MisdemeanourContext.Provider>
-)
-    }
+        </Routes>    
 
 export default Router;
