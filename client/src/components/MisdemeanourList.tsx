e09cc65cd06b0ea1679ca84ab458238b00aa98f2
katiek78 <kermodekatie@gmail.com>
2023-03-30 17:11:58 +0100

    feat: Display self-confessed misdemeanours in green

--- client/src/components/MisdemeanourList.tsx
+++ client/src/components/MisdemeanourList.tsx
@@ -12,11 +12,11 @@ const MisdemeanourList: React.FC = () => {
     if (!selectBox) return true;
     return selectBox.value==='all' || selectBox.value === '' || selectBox.value===m.misdemeanour})
 .map((m, index) => (<React.Fragment key={'gridItem' + index}>
-<p className='grid__item grid__line grid__column1'>{m.citizenId}</p>
-<p className='grid__item grid__line grid__column2'>{m.date}</p>
-<p className='grid__item grid__line grid__column3'><span className='misdemeanour__kind'>{m.misdemeanour} </span>{MISDEMEANOUR_EMOJIS[MISDEMEANOURS.indexOf(m.misdemeanour)]}</p>
-<p className='grid__item grid__line grid__column4'>{m.details}</p>
-<p className='grid__item grid__column5'><img src={`https://picsum.photos/150/90?random=${m.citizenId}`} alt="Lorem Picsum image" /></p>
+<p className={`grid__item grid__line grid__column1`}><span className={`${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.citizenId}</span></p>
+<p className={`grid__item grid__line grid__column2`}><span className={`${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.date}</span></p>
+<p className={`grid__item grid__line grid__column3`}><span className={`misdemeanour__kind ${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.misdemeanour} </span>{MISDEMEANOUR_EMOJIS[MISDEMEANOURS.indexOf(m.misdemeanour)]}</p>
+<p className={`grid__item grid__line grid__column4`}><span className={`misdemeanour__kind ${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.details}</span></p>
+<p className={`grid__item grid__column5`}><img src={`https://picsum.photos/150/90?random=${m.citizenId}`} alt="Lorem Picsum image" /></p>
 </React.Fragment>))}   
 </>
     );
