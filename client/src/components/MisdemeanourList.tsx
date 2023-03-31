766ad22cd9478471fb8da640ef9be002eceaac5b
katiek78 <kermodekatie@gmail.com>
2023-03-31 08:06:20 +0100

    feat: Forgiven text is more descriptive and LFC photo shows as punishment for 'united' misdemeanour

--- client/src/components/MisdemeanourList.tsx
+++ client/src/components/MisdemeanourList.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useMisdemeanours } from "./MisdemeanourContext";
-import { MISDEMEANOURS, MISDEMEANOUR_EMOJIS } from "../types/misdemeanours.types";
+import { MISDEMEANOURS, MISDEMEANOUR_EMOJIS, MISDEMEANOUR_LONG_TEXTS } from "../types/misdemeanours.types";
 import pigeon from '../assets/icons8-peace-pigeon-80.png'
+import lfc from '../assets/soccer-g374b8d6df_640.jpg'
 
 const MisdemeanourList: React.FC = () => {
     const { misdemeanours, setMisdemeanours } = useMisdemeanours();
@@ -10,7 +11,7 @@ const MisdemeanourList: React.FC = () => {
         setMisdemeanours(misdemeanours.map(m => {
             if (m.citizenId === citizenId) {
                 if (m.forgivenessesNeeded === 1) {
-                    alert(`Citizen ${m.citizenId} forgiven for ${m.misdemeanour}`)
+                    alert(`Citizen ${m.citizenId} forgiven for ${MISDEMEANOUR_LONG_TEXTS[MISDEMEANOURS.indexOf(m.misdemeanour)]}`)
                 }
                 return {...m, forgivenessesNeeded: m.forgivenessesNeeded ? m.forgivenessesNeeded - 1 : null};
             } else return m;
@@ -31,7 +32,7 @@ const MisdemeanourList: React.FC = () => {
 <p className={`grid__item grid__line grid__column2`}><span className={`${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.date}</span></p>
 <p className={`grid__item grid__line grid__column3`}><span className={`misdemeanour__kind ${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.misdemeanour} </span>{MISDEMEANOUR_EMOJIS[MISDEMEANOURS.indexOf(m.misdemeanour)]}</p>
 <p className={`grid__item grid__line grid__column4`}><span className={`{m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.details}</span></p>
-<p className={`grid__item grid__line grid__column5`}><img src={`https://picsum.photos/150/90?random=${m.citizenId}`} alt="Lorem Picsum image" /></p>
+<p className={`grid__item grid__line grid__column5`}><img src={m.misdemeanour === 'united' ? lfc : `https://picsum.photos/150/90?random=${m.citizenId}`} alt="Lorem Picsum image" /></p>
 <p className={`grid__item grid__column6`}><span className={`${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.forgivenessesNeeded} {m.forgivenessesNeeded !== null && m.forgivenessesNeeded > 0 && <img onClick={(e) => handleForgive(m.citizenId)} height={30} alt="peace pigeon icon" src={pigeon} />}</span></p>
 </React.Fragment>))}   
 </>
--- client/src/types/misdemeanours.types.ts
+++ client/src/types/misdemeanours.types.ts
@@ -1,36 +1,38 @@
 export const MISDEMEANOURS = [
-	'rudeness',
-	'vegetables',
-	'lift',
-	'united',
+  "rudeness",
+  "vegetables",
+  "lift",
+  "united",
 ] as const;
 export type MisdemeanourKind = (typeof MISDEMEANOURS)[number];
 
-export const JUST_TALK = 'just-talk';
+export const JUST_TALK = "just-talk";
 export type JustTalk = typeof JUST_TALK;
 
 export type Misdemeanour = {
-	citizenId: number;
-	misdemeanour: MisdemeanourKind;
-	date: string; // we'll stringify this for easy sending via HTTP rather than storing the full Date object
-	details?: string;
-	forgivenessesNeeded: number | null;
+  citizenId: number;
+  misdemeanour: MisdemeanourKind;
+  date: string; // we'll stringify this for easy sending via HTTP rather than storing the full Date object
+  details?: string;
+  forgivenessesNeeded: number | null;
 };
 
-export const MISDEMEANOUR_EMOJIS = [
-	'🤪',	
-	'🥗',
-	'🗣',
-	'😈'
-]
+export const MISDEMEANOUR_EMOJIS = ["🤪", "🥗", "🗣", "😈"] as const;
+
+export const MISDEMEANOUR_LONG_TEXTS = [
+  "rudeness",
+  "not eating vegetables",
+  "talking in a lift",
+  "supporting United",
+] as const;
 
 export function isMisdemeanourKindorJustTalk(
-	userInput: string
-  ): userInput is MisdemeanourKind | JustTalk {
-	return (
-	  MISDEMEANOURS.find((el) => el === userInput) !== undefined ||
-	  userInput === JUST_TALK
-	);
-  }
+  userInput: string
+): userInput is MisdemeanourKind | JustTalk {
+  return (
+    MISDEMEANOURS.find((el) => el === userInput) !== undefined ||
+    userInput === JUST_TALK
+  );
+}
 
 export const STANDARD_FORGIVENESSES = 10;
