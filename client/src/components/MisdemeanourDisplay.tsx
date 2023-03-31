0e5c39cbf6a87dc64d514b90287a7bc25eab3581
katiek78 <kermodekatie@gmail.com>
2023-03-31 13:07:20 +0100

    refactor: Separate off MisdemeanourTable component

--- client/src/components/MisdemeanourDisplay.tsx
+++ client/src/components/MisdemeanourDisplay.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { STANDARD_FORGIVENESSES } from "../types/misdemeanours.types";
-import MisdemeanourFilter from "./MisdemeanourFilter";
-import MisdemeanourList from "./MisdemeanourList";
+import MisdemeanourTable from "./MisdemeanourTable";
 import pigeon from '../assets/icons8-peace-pigeon-80.png'
 
 const MisdemeanourDisplay : React.FC = () => {    
-    const [ filterKind, setFilterKind ] = useState<string>('');    
 
     return(
     <>
@@ -15,21 +13,7 @@ const MisdemeanourDisplay : React.FC = () => {
         <p className="misdemeanour__message">For United fans there can be <strong>no forgiveness</strong> until you perform a Liverpool song of our choice at a public ceremony - contact us for details.</p>
         </details>
     </div>
-    <div className="misdemeanour__container container">
-        <p className='misdemeanour__filter'>
-            <MisdemeanourFilter filterKind={filterKind} setFilterKind={setFilterKind} />
-    </p></div>
-    <div id='misdemeanourTable' className='misdemeanour__container container'>                      
-            <p className='grid__line heading citizenId__heading'>Citizen ID</p>                  
-            <p className='grid__line heading date__heading'>Date</p>                    
-            
-            <p className='grid__line heading misdemeanour__heading'>Misdemeanour</p>            
-            <p className='grid__line heading details__heading'>Details</p>            
-            <p className='grid__line heading punishment__heading'>Punishment idea</p>
-            <p className='heading forgivenesses__heading'>Forgivenesses needed</p>
-            
-            <MisdemeanourList />
-    </div>
+    <MisdemeanourTable />
 
     <div>
     <a target="_blank" href="https://icons8.com/icon/7mHfZHK9mvZw/peace-pigeon">Peace Pigeon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
---
+++ client/src/components/MisdemeanourTable.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import MisdemeanourList from "./MisdemeanourList";
+import MisdemeanourFilter from "./MisdemeanourFilter";
+
+const MisdemeanourTable: React.FC = () => {
+  const [filterKind, setFilterKind] = useState<string>("");
+  return (
+    <>
+      <div className="misdemeanour__container container">
+        <p className="misdemeanour__filter">
+          <MisdemeanourFilter
+            filterKind={filterKind}
+            setFilterKind={setFilterKind}
+          />
+        </p>
+      </div>
+      <div id="misdemeanourTable" className="misdemeanour__container container">
+        <p className="grid__line heading citizenId__heading">Citizen ID</p>
+        <p className="grid__line heading date__heading">Date</p>
+
+        <p className="grid__line heading misdemeanour__heading">Misdemeanour</p>
+        <p className="grid__line heading details__heading">Details</p>
+        <p className="grid__line heading punishment__heading">
+          Punishment idea
+        </p>
+        <p className="heading forgivenesses__heading">Forgivenesses needed</p>
+
+        <MisdemeanourList />
+      </div>
+    </>
+  );
+};
+
+export default MisdemeanourTable;
