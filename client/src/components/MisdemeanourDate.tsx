85985643f14a16808200d1daa89b3500d7fc2174
katiek78 <kermodekatie@gmail.com>
2023-03-31 12:13:13 +0100

    refactor: Separate off MisdemeanourDate component

---
+++ client/src/components/MisdemeanourDate.tsx
@@ -0,0 +1,22 @@
+import { Misdemeanour } from "../types/misdemeanours.types";
+
+interface MisdemeanourDateProps {
+  misdemeanour: Misdemeanour;
+}
+
+const MisdemeanourDate: React.FC<MisdemeanourDateProps> = ({
+  misdemeanour,
+}) => {
+  return (
+    <p className={`grid__item grid__line grid__column2`}>
+      <span
+        className={`${
+          misdemeanour.details ? "misdemeanour--self-confessed" : ""
+        }`}
+      >
+        {misdemeanour.date}
+      </span>
+    </p>
+  );
+};
+export default MisdemeanourDate;
--- client/src/components/MisdemeanourList.tsx
+++ client/src/components/MisdemeanourList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useMisdemeanours } from "./MisdemeanourContext";
 import { MISDEMEANOURS, MISDEMEANOUR_EMOJIS, MISDEMEANOUR_LONG_TEXTS } from "../types/misdemeanours.types";
 import MisdemeanourCitizenID from "./MisdemeanourCitizenID";
+import MisdemeanourDate from "./MisdemeanourDate";
 import pigeon from '../assets/icons8-peace-pigeon-80.png'
 import lfc from '../assets/soccer-g374b8d6df_640.jpg'
 
@@ -29,7 +30,7 @@ const MisdemeanourList: React.FC = () => {
     return selectBox.value==='all' || selectBox.value === '' || selectBox.value===m.misdemeanour})
 .map((m, index) => (<React.Fragment key={'gridItem' + index}>
 <MisdemeanourCitizenID misdemeanour={m} />
-<p className={`grid__item grid__line grid__column2`}><span className={`${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.date}</span></p>
+<MisdemeanourDate misdemeanour={m} />
 <p className={`grid__item grid__line grid__column3`}><span className={`misdemeanour__kind ${m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.misdemeanour} </span>{MISDEMEANOUR_EMOJIS[MISDEMEANOURS.indexOf(m.misdemeanour)]}</p>
 <p className={`grid__item grid__line grid__column4`}><span className={`{m.details ? 'misdemeanour--self-confessed' : ''}`}>{m.details}</span></p>
 <p className={`grid__item grid__line grid__column5`}><img src={m.misdemeanour === 'united' ? lfc : `https://picsum.photos/150/90?random=${m.citizenId}`} alt="Lorem Picsum image" /></p>
