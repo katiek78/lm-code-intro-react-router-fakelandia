2ba8bdabce5de9ea79b6152d6de559c2355546ea
katiek78 <kermodekatie@gmail.com>
2023-04-03 16:43:00 +0100

    feat: Change styling of word 'Liverpool'

--- client/src/components/MisdemeanourDisplay.tsx
+++ client/src/components/MisdemeanourDisplay.tsx
@@ -10,7 +10,7 @@ const MisdemeanourDisplay : React.FC = () => {
     <div className="container">
         <details><summary><strong>NEW!</strong> Forgiveness for Fakelandia citizens</summary><p className="misdemeanour__message">As we want to be a fair society, we've added the possibility of <strong>forgiving</strong> your fellow citizens. Click on the pigeon of peace (<img src={pigeon} height={20}></img>) to forgive a misdemeanour.</p>
         <p className="misdemeanour__message">Misdemeanours normally require <strong>{STANDARD_FORGIVENESSES}</strong> forgivenesses, but this is reduced to <strong>{Math.floor(STANDARD_FORGIVENESSES/2)}</strong> if you confess!</p>
-        <p className="misdemeanour__message">For United fans there can be <strong>no forgiveness</strong> until you perform a Liverpool song of our choice at a public ceremony - contact us for details.</p>
+        <p className="misdemeanour__message">For United fans there can be <strong>no forgiveness</strong> until you perform a <span className="text--lfc">Liverpool</span> song of our choice at a public ceremony - contact us for details.</p>
         </details>
     </div>
     <MisdemeanourTable />
