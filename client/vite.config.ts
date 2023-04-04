21e2852b0e30255c75e4789b7c116615b40e151e
katiek78 <kermodekatie@gmail.com>
2023-04-04 19:48:09 +0100

    feat: modify tsconfig to get it to work with GitHub Pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tweak/",
+  base: "/lm-code-intro-react-router-fakelandia/",
 })
