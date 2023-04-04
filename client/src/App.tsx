a7f4376e7698fa1d6c8d22a90e34fbc9db914182
katiek78 <kermodekatie@gmail.com>
2023-04-04 18:09:30 +0100

    fix: change way in which HashRouter is used to make it work on GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,10 @@
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
-import { HashRouter } from 'react-router-dom'
-import { ErrorBoundary } from 'react-error-boundary'
-import { ErrorFallback } from './components/error/ErrorFallback'
 import Router from './components/Router'
 
 function App() {
 
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <HashRouter>
         <Router />        
-      </HashRouter>
-    </ErrorBoundary>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
+import { ErrorFallback } from './components/error/ErrorFallback'
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <RouterProvider router={router} />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
